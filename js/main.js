771f8ca818a2286a0d5f84b49bdb5c0464dc9849
Hrolfr <sabrina.sales163@gmail.com>
2020-08-24 17:50:34 -0300

    primeira tentativa

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 /*var nome= "Rafael Galleani"; 
 alert (`Bem vindo ${nome}` ); */ 
 
-var nome= "Rafael Galleani"; var idade= 29;
-alert (`Bem vindo ${nome} ${idade}` );
+/*var nome= "Rafael Galleani"; var idade= 29;
+alert (`Bem vindo ${nome} ${idade}` );*/
+
+let sabrina= 'tchola';
+console.log ('A Sabrina é tchola');
+
+if (true){
+console.log ('a sabrina é tchola ao quadrado')
+}
