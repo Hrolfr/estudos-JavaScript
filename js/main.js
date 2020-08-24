3e0e946d42d1f37fdc6f4ca1380130daecd0ca31
Hrolfr <sabrina.sales163@gmail.com>
2020-08-24 18:00:00 -0300

    var idade

--- js/main.js
+++ js/main.js
@@ -4,9 +4,28 @@ alert (`Bem vindo ${nome}` ); */
 /*var nome= "Rafael Galleani"; var idade= 29;
 alert (`Bem vindo ${nome} ${idade}` );*/
 
-let sabrina= 'tchola';
-console.log ('A Sabrina é tchola');
+/*newFunction();
+
+function newFunction() {
+    newFunction_1();
+
+    if (true) {
+        console.log('a sabrina é tchola ao quadrado');
+    }
+
 
 if (true){
 console.log ('a sabrina é tchola ao quadrado')
-}
+}*/
+
+
+   /* var idade = 29;
+    var idade2 = 10;
+    alert(idade + idade2);
+
+    var idade= "12"
+    var idade2= "125"
+    alert (idade + idade2)*/
+
+
+
