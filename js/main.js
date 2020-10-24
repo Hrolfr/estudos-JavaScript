bc85e7f482b946a05312fdf0bb272492d9dacfe7
Hrolfr <sabrina.sales163@gmail.com>
2020-10-24 11:50:27 -0300

    algumas mudanças nos coments

--- js/main.js
+++ js/main.js
@@ -1,37 +1,46 @@
-/*var nome= "Rafael Galleani"; 
-alert (`Bem vindo ${nome}` ); */ 
+//Exemplos do Rafael Galleani (professor)
 
-/*var nome= "Rafael Galleani"; var idade= 29;
-alert (`Bem vindo ${nome} ${idade}` );*/
+//Exemplo 1
+var nome= "Rafael Galleani"; 
+alert (`Bem vindo ${nome}` ); 
 
-/*newFunction();
+//Exemplo 2
+var nome= "Rafael Galleani"; var idade= 29;
+alert (`Bem vindo ${nome} ${idade}` );
 
-function newFunction() {
+
+//Exemplo 3 
+newFunction();
+
+ function newFunction() {
     newFunction_1();
 
     if (true) {
-        console.log('a sabrina é tchola ao quadrado');
+     console.log('a sabrina é tchola ao quadrado');
     }
 
 
-if (true){
-console.log ('a sabrina é tchola ao quadrado')
-}*/
+    if (true) {
+     console.log ('a sabrina é tchola ao quadrado');
+    }
 
-
-   /* var idade = 29;
+    //Exemplo 4
+    var idade = 29;
     var idade2 = 10;
     alert(idade + idade2);
 
+    //Exemplo 5
     var idade= "12"
     var idade2= "125"
-    alert (idade + idade2)*/
+    alert (idade + idade2)
 
-    /*var number= 25;
+    //Exemplo 6
+    var number= 25;
     var numer2= 51;
-    alert (number + numer2)*/
-
-  /* var fruta = "melao";
+    alert (number + numer2)
+  
+    //Exemplo 7
+   var fruta = "melao";
    var fruta2 = "laranja";
    alert ("delicia"  +  fruta  +  fruta2);
    typeof (melao);*/
