bfc48d9c2923bb011c160ad284eed1528163a2f6
Hrolfr <sabrina.sales163@gmail.com>
2020-10-24 12:21:20 -0300

    fiz até hoisting de função

--- js/newFunction.1.js
+++ js/newFunction.1.js
@@ -5,3 +5,45 @@
 function soma (a,b){
 return a+b;
 }
+
+//Função currying
+
+function soma (a){
+    return function (b){
+        return a+b;
+    }
+   
+   }
+   const soma2= soma (2)
+   console.log(soma2(2));
+   console.log(soma2(3));
+   console.log(soma2(4));
+   console.log(soma2(5));
+
+   /*Retornou 4:
+    ƒ (b){
+        return a+b;
+    }*/
+
+//Hoisting de variáveis
+
+function fn(){
+    console.log (text);
+    var text='Exemplo';
+    console.log (text);
+}
+fn();
+
+//Devolveu "Exemplo e 2 undefineds"
+
+//Hoisting de função
+function fn(){
+    log('Hoisting de função')
+        function log (value){
+        console.log(value);
+    }
+}
+fn();
+
+//Devolve Hoisting de função
+
