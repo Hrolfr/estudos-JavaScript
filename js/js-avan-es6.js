afd2b985b3f4309a3e89e98d4502ef1e4c25213a
Hrolfr <sabrina.sales163@gmail.com>
2021-04-22 11:34:15 -0300

    array const animGrand

--- js/js-avan-es6.js
+++ js/js-avan-es6.js
@@ -1,9 +1,19 @@
 
 // spread em array e string 22/04
 
-const string = 'Ost er god';
+/*const marca = 'Digital Innovation One';
 
-function logArgs(args){
+function logArgs(...args){
     console.log(args);
 }
-logArgs(...string);
+logArgs(...marca); gera: (22) ["D", "i", "g", "i", "t", "a", "l", " ", "I", "n", "n", "o", "v", "a", "t", "i", "o", "n", " ", "O", "n", "e"]*/
+
+//array:
+
+/*const animGrand = ['leão', 'tigre', 'onça'];
+
+ function join (a, b, c) {
+   console.log(a, b, c);
+}
+const animGrand2 = animGrand.concat(['guepardo', 'leopardo', 'pantera']);
+   console.log(animGrand2); (6) ["leão", "tigre", "onça", "guepardo", "leopardo", "pantera"]*/
